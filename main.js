09773a17aa6a8bc89f69132cc38cea771515ed2a
Ladev25 <libanmohamud22@gmail.com>
2020-09-24 15:51:56 +0000

    Add a box for multiplication

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ let total = firstNumber + secondNumber;
 
 
 let primary = document.getElementById('num1');
-    primary.innerHTML = `<p>${firstNumber}</p>`;
-
+    primary.innerHTML = `${firstNumber}`;
+    
 let secondary = document.getElementById('num2');
-    secondary.innerHTML = `<p>${secondNumber}</p>`
+    secondary.innerHTML = `${secondNumber}`;
 
 
 
@@ -17,14 +17,53 @@ let button = document.getElementById('btn')
 
 button.addEventListener('click', function(){
 
-let guess = document.getElementById('guess').value;
-    guess = Number(guess);
+let guessaddition = document.getElementById('guessaddition').value;
+    guessaddition = Number(guessaddition);
 
-if (guess === total){
+if (guessaddition === total){
     alert('That is Correct!');
     window.location.reload()
 } else {
     alert('Sorry. Incorrect. The correct answer was ' + total + '.')
+    window.location.reload()
+
+}
+    });
+
+
+
+
+
+
+
+// Multiplication game
+
+let firstNumbers = parseInt(Math.random()*10);
+let secondNumbers = parseInt(Math.random()*10);
+
+
+let totaltwo = firstNumbers * secondNumbers;
+
+    let primarytwo = document.getElementById('num3');
+    primarytwo.innerHTML = `${firstNumbers}`;
+    
+let secondarytwo = document.getElementById('num4');
+    secondarytwo.innerHTML = `${secondNumbers}`;
+
+
+
+let buttontwo = document.getElementById('btnmultiplication')
+
+buttontwo.addEventListener('click', function(){
+
+let guessmultiplication = document.getElementById('guessmultiplication').value;
+    guessmultiplication = Number(guessmultiplication);
+
+if (guessmultiplication === totaltwo){
+    alert('That is Correct!');
+    window.location.reload()
+} else {
+    alert('Sorry. Incorrect. The correct answer was ' + totaltwo + '.')
     window.location.reload()
 
 }
