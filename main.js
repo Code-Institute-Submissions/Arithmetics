b58775aed361e857ea0abfcd5538a8cd27375974
Ladev25 <libanmohamud22@gmail.com>
2020-09-26 15:49:59 +0000

    Restructure my javascript code

--- main.js
+++ main.js
@@ -1,70 +1,105 @@
-let firstNumber = parseInt(Math.random()*10);
-let secondNumber = parseInt(Math.random()*10);
+"use strict";
+
+let total = 0;
+let totalTwo = 0;
+const ADDITION_OPERATION = "addition";
+const MULTIPLICATION_OPERATION = "multiplication";
 
 
-let total = firstNumber + secondNumber;
+function resetNumbers() {
+    generateNumbers("num1", "num2", ADDITION_OPERATION);
+    generateNumbers("num3", "num4", MULTIPLICATION_OPERATION);
+    clearAnswers();
+}
 
+function clearAnswers() {
+    document.getElementById("guessaddition").value = "";
+    document.getElementById("guessmultiplication").value = "";
+}
 
-let primary = document.getElementById('num1');
+/**
+* Generates randon numbers for the game
+* @param {String} elemOne  Html element Id to set the first random number.
+* @param {String} elemTwo  Html element Id to set the second random number.
+* @param {String} operationType  name of arithematic operation. e.g. addition.
+**/
+function generateNumbers(elemOne, elemTwo, operationType) {
+    let firstNumber = parseInt(Math.random()*10);
+    let secondNumber = parseInt(Math.random()*10);
+
+    if (operationType === MULTIPLICATION_OPERATION) {
+        totalTwo = firstNumber * secondNumber;
+    } else if (operationType === ADDITION_OPERATION) {
+       total = firstNumber + secondNumber;
+    }
+    let primary = document.getElementById(elemOne);
     primary.innerHTML = `${firstNumber}`;
-    
-let secondary = document.getElementById('num2');
+   
+    let secondary = document.getElementById(elemTwo);
     secondary.innerHTML = `${secondNumber}`;
 
-
-
-let button = document.getElementById('btn')
-
-button.addEventListener('click', function(){
-
-let guessaddition = document.getElementById('guessaddition').value;
-    guessaddition = Number(guessaddition);
-
-if (guessaddition === total){
-    alert('That is Correct!');
-    window.location.reload()
-} else {
-    alert('Sorry. Incorrect. The correct answer was ' + total + '.')
-    window.location.reload()
-
 }
-    });
+
+function validateAnswer(event) {
 
 
+ let operation  = getOperationToValidate(event.currentTarget.id)
+   let answer = getAnswer(operation)
 
+    if (isAnswerValid(operation, answer)){
+        alert("That is Correct!");
+        resetNumbers();
+    } else {
+        alert('Sorry. Incorrect. The correct answer was ' + total + '.')
+        //window.location.reload()
+        resetNumbers();
 
-
-
-
-// Multiplication game
-
-let firstNumbers = parseInt(Math.random()*10);
-let secondNumbers = parseInt(Math.random()*10);
-
-
-let totaltwo = firstNumbers * secondNumbers;
-
-    let primarytwo = document.getElementById('num3');
-    primarytwo.innerHTML = `${firstNumbers}`;
-    
-let secondarytwo = document.getElementById('num4');
-    secondarytwo.innerHTML = `${secondNumbers}`;
-
-
-
-let buttontwo = document.getElementById('btnmultiplication')
-
-buttontwo.addEventListener('click', function(){
-
-let guessmultiplication = document.getElementById('guessmultiplication').value;
-    guessmultiplication = Number(guessmultiplication);
-
-if (guessmultiplication === totaltwo){
-    alert('That is Correct!');
-    window.location.reload()
-} else {
-    alert('Sorry. Incorrect. The correct answer was ' + totaltwo + '.')
-    window.location.reload()
-
+    }
 }
-    });
+
+/**
+* Fetches the user input by the arthimatic operation.
+* @param {String} operation  name of arithematic operation. e.g. addition.
+* @returns {Number} Number added by user.
+**/
+function getAnswer(operation) {
+    if (operation === ADDITION_OPERATION) {
+        let guessaddition = document.getElementById('guessaddition').value;
+        return  Number(guessaddition);
+    } else if (operation == MULTIPLICATION_OPERATION) {
+        let guessmultiplication = document.getElementById('guessmultiplication').value;
+        return Number(guessmultiplication);
+    }
+    return -1;
+}
+
+function getOperationToValidate(btnId) {
+    if (btnId === "btn") {
+        return ADDITION_OPERATION;
+    } else if (btnId == "btnmultiplication") {
+        return MULTIPLICATION_OPERATION;
+    }
+    return null;
+}
+
+function isAnswerValid(elemId, answer) {
+    if (elemId === ADDITION_OPERATION) {
+        return answer === total;
+    } else if (elemId === MULTIPLICATION_OPERATION) {
+        return answer === totalTwo;
+    } else {
+       return false;
+    }
+}
+
+(function() {
+   
+    resetNumbers();
+    let button = document.getElementById('btn')
+    button.addEventListener('click', validateAnswer);
+
+    let buttontwo = document.getElementById('btnmultiplication')
+    buttontwo.addEventListener('click', validateAnswer);
+
+
+})();
